c6e1c0dce0ccb4eaf38c38e577a9ba0e20af6d1f
dfarr <dfarrelly1@outlook.com>
2020-10-02 04:29:55 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC7I9IOI7JSdKYANodVSfTZ-UcBgHH4bkU",
+    authDomain: "wound-doctor-74a17.firebaseapp.com",
+    databaseURL: "https://wound-doctor-74a17.firebaseio.com",
+    projectId: "wound-doctor-74a17",
+    storageBucket: "wound-doctor-74a17.appspot.com",
+    messagingSenderId: "103407778222",
+    appId: "1:103407778222:web:80eecd45138d2a5bcfb03f"
+  }
 };
 
 /*
